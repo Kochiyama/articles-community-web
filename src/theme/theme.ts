1c43eb183222901d7c42c975253c4c710b777ef2
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-10 11:33:07 -0400

    Chore: create color scheme

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+	foreground: {
+		100: '#161616',
+		200: '#444444',
+	},
+	background: {
+		100: '#EDEDED',
+		200: '#C7C7C7',
+	},
+	brand: {
+		100: '#2AAB87',
+	},
+	error: {
+		100: '#C54141',
+	},
+}
+
+export const theme = extendTheme({ colors })
