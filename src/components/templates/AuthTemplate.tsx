15cd6ad2e07dfb5cf9564f1ee9b04ec284e53a5b
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-11 14:16:44 -0400

    Refactor: add responsivity

--- src/components/templates/AuthTemplate.tsx
+++ src/components/templates/AuthTemplate.tsx
@@ -1,23 +1,38 @@
-import { Center, Container, Flex, Image } from '@chakra-ui/react'
+import {
+	Center,
+	Container,
+	Flex,
+	Image,
+	useBreakpointValue,
+} from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 interface Props {
 	children: ReactNode | ReactNode[]
 }
 
-export const AuthTemplate = ({ children }: Props) => (
-	<Flex p='2rem' h='100vh'>
-		<Center flex='1' mr='2rem' px='4rem'>
-			<Flex flexDir='column' minW='20rem'>
-				{children}
-			</Flex>
-		</Center>
+export const AuthTemplate = ({ children }: Props) => {
+	const isWideVersion = useBreakpointValue({
+		md: true,
+		base: false,
+	})
 
-		<Flex flex='2'>
-			<Image
-				src='/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png'
-				alt='Image by Glenn Carstens Peters Unsplash'
-			/>
+	return (
+		<Flex p='2rem' h='100vh'>
+			<Center flex='1' px={{ base: '0rem', md: '4rem' }}>
+				<Flex flexDir='column' minW='20rem'>
+					{children}
+				</Flex>
+			</Center>
+
+			{isWideVersion && (
+				<Flex flex='2'>
+					<Image
+						src='/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png'
+						alt='Image by Glenn Carstens Peters Unsplash'
+					/>
+				</Flex>
+			)}
 		</Flex>
-	</Flex>
-)
+	)
+}
