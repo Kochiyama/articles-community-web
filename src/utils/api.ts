490b289ff5537ee92e940f77bb8436b6bcaf8d84
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-12 20:28:50 -0400

    Regactor: add dynamic baseURL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,22 @@
 import axios from 'axios'
 
+console.log(process.env.NODE_ENV)
+let baseURL = ''
+
+switch (process.env.NODE_ENV) {
+	case 'development':
+		baseURL = 'http://localhost:3030'
+		break
+
+	case 'test':
+		baseURL = 'https://article-community.herokuapp.com'
+		break
+
+	case 'production':
+		baseURL = 'https://article-community.herokuapp.com'
+		break
+}
+
 export const api = axios.create({
-	baseURL: 'http://localhost:3030',
+	baseURL,
 })
