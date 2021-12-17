c584c0012259d54fea24513609cd7b7e5ef4013f
Kochiyama <marcelokochiyama2210@gmail.com>
2021-12-16 21:29:17 -0400

    Refactor: show app template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,18 @@
 import { Button, Center, Image } from '@chakra-ui/react'
 import type { GetServerSideProps, NextPage } from 'next'
 import { parseCookies } from 'nookies'
+import { AppTemplate } from '../components/templates/AppTemplate'
 import { cookieSettings } from '../constants/cookies'
 import { useAuth } from '../contexts/AuthContext'
 
 const Home: NextPage = () => {
-	const { logout } = useAuth()
-
 	return (
-		<Center h='100vh'>
-			<Button onClick={() => logout()}>Log out</Button>
-			<Image w='20rem' src='/logo.png' alt='Articles Community Logo' />
-		</Center>
+		<AppTemplate />
+		// <Center h='100vh'>
+		// 	<Button onClick={() => logout()}>Log out</Button>
+		// 	<Image w='20rem' src='/logo.png' alt='Articles Community Logo' />
+		// </Center>
 	)
-}
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-	const token = parseCookies(ctx)[cookieSettings.TOKEN_KEY]
-
-	if (!token) {
-		return {
-			redirect: {
-				destination: '/login',
-				permanent: false,
-			},
-		}
-	}
-
-	return {
-		props: {},
-	}
 }
 
 export default Home
